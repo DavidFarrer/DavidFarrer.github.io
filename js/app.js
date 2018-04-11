a7a07df0c683163a45fe1001648b82a06b9baa99
Dave Farrer <dave.farrer89@gmail.com>
2018-04-11 16:51:44 -0400

    fix scroll bug

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 var navbar = document.querySelector(".navbar");
 var logo = document.getElementById("logo");
 
+// Typed.js for typing simulation
 var typed = new Typed("#typed", {
   strings: ["Hi...", "My name is Dave.", "Web development is my passion."],
   typeSpeed: 100,
@@ -41,97 +42,89 @@ $('a[href*="#"]')
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       // Does a scroll target exist?
       if (target.length) {
-        // Only prevent default if animation is actually gonna happen
+        // Only prevent default if animation is actually going to occur
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top - 70
-        }, 1000, 'swing', function() {
-          //Callback after animation
-          //Must change focus!
-          // var $target = $(target);
-          // $target.focus();
-          // if ($target.is(":focus")) { // Checking if the target was focused
-          //   return false;
-          // } else {
-          //   $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          //   $target.focus(); // Set focus again
-          // };
-        });
+        }, 1000, 'swing');
       }
     }
   });
 
-  (function navbarSwitcher() {
-    var aboutSection = document.querySelector("#about");
-    var projectsSection = document.querySelector("#projects");
-    var contactSection = document.querySelector("#contact");
-    var navLinks = document.querySelectorAll(".navlink");
-    var theme;
-    var HOME = "home";
-    var ABOUT = "about";
-    var PROJECTS = "projects";
-    var CONTACT = "contact";
-    var new_theme;
 
-    var scrolling = false;
+// Scroll Handler IIFE
+(function navbarSwitcher() {
+  var aboutSection = document.querySelector("#about");
+  var projectsSection = document.querySelector("#projects");
+  var contactSection = document.querySelector("#contact");
+  var navLinks = document.querySelectorAll(".navlink");
+  var theme;
+  var HOME = "home";
+  var ABOUT = "about";
+  var PROJECTS = "projects";
+  var CONTACT = "contact";
+  var new_theme;
 
-    function highlightLink(index) {
-      navLinks.forEach(function(link, i) {
-        if (i === index) {
-          link.classList.add("active");
-        } else {
-          link.classList.remove("active");
-        }
-      });
-    }
+  var scrolling = false;
 
-    var scroll_handler = function() {
-      return setTimeout(function() {
-        if (scrolling) return;
-        var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-        if (scrollTop < aboutSection.offsetTop - 74) {
-          new_theme = HOME;
-        } else if (scrollTop < projectsSection.offsetTop - 74) {
-          new_theme = ABOUT;
-        } else if (scrollTop < contactSection.offsetTop - 74) {
-          new_theme = PROJECTS;
-        } else {
-          new_theme = CONTACT;
-        }
+  function highlightLink(index) {
+    navLinks.forEach(function(link, i) {
+      if (i === index) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  }
 
-        if (new_theme === theme) {
-          scrolling = false;
-          return;
-        }
+  var scroll_handler = function() {
+    return setTimeout(function() {
+      if (scrolling) return;
+      var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+      if (scrollTop < aboutSection.offsetTop - 74) {
+        new_theme = HOME;
+      } else if (scrollTop < projectsSection.offsetTop - 74) {
+        new_theme = ABOUT;
+      } else if (scrollTop < contactSection.offsetTop - 74) {
+        new_theme = PROJECTS;
+      } else {
+        new_theme = CONTACT;
+      }
 
-        switch (new_theme) {
-          case HOME:
-            navbar.classList.remove("navbar-show");
-            logo.classList.remove("logo-show");
-            highlightLink(0);
-            break;
-          case ABOUT:
-            navbar.classList.add("navbar-show");
-            logo.classList.add("logo-show");
-            highlightLink(1);
-            break;
-          case PROJECTS:
-            navbar.classList.add("navbar-show");
-            logo.classList.add("logo-show");
-            highlightLink(2);
-            break;
-          default:
-            navbar.classList.add("navbar-show");
-            logo.classList.add("logo-show");
-            highlightLink(3);
-        }
-
-        theme = new_theme;
+      if (new_theme === theme) { // Don't repaint if theme hasn't changed
         scrolling = false;
-      });
-    }
-    window.addEventListener("scroll", scroll_handler);
-    window.addEventListener("load", scroll_handler);
-  })();
+        return;
+      }
 
-  document.querySelector("#year").textContent = new Date().getFullYear();
+      switch (new_theme) {
+        case HOME:
+          navbar.classList.remove("navbar-show");
+          logo.classList.remove("logo-show");
+          highlightLink(0);
+          break;
+        case ABOUT:
+          navbar.classList.add("navbar-show");
+          logo.classList.add("logo-show");
+          highlightLink(1);
+          break;
+        case PROJECTS:
+          navbar.classList.add("navbar-show");
+          logo.classList.add("logo-show");
+          highlightLink(2);
+          break;
+        default:
+          navbar.classList.add("navbar-show");
+          logo.classList.add("logo-show");
+          highlightLink(3);
+      }
+
+      theme = new_theme;
+      scrolling = false;
+    });
+  }
+  window.addEventListener("scroll", scroll_handler);
+  window.addEventListener("load", scroll_handler);
+})();
+
+// Dynamic year for copyright section
+document.querySelector("#year").textContent = new Date().getFullYear();
